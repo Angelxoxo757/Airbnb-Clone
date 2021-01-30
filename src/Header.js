b4aaa37c38a27de4fd0dbfd47f5670e8dfd26d48
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-01-29 19:11:12 -0500

    Added Airbnb Clone Material-UI

--- src/Header.js
+++ src/Header.js
@@ -9,6 +9,10 @@ function Header() {
         src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
         alt="Airbnb Logo"
       />
+      <div className="header-center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
     </div>
   );
 }
