5693fa455cc4f403d2460b574bc2cb437ed986a7
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-01-30 13:11:27 -0500

    Working on Airbnb Clone Home Component

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return <div className="hone">{/* <h1>Home Component</h1> */}</div>;
+  return (
+    <div className="hone">
+      <h1>Home Component</h1>
+    </div>
+  );
 }
 
 export default Home;
