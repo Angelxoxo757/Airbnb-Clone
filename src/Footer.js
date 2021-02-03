11beb647cba589684aca63ab73050def1f91ddb2
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-02-02 21:19:32 -0500

    Finished Working on Airbnb Clone Card and Footer Component

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        © 2021 Airbnb Clone! By Maria Cromety. No rights reserved this is a
+        demo.
+      </p>
+      <p>Privacy - Terns - Sitemap - Company Details</p>
+    </div>
+  );
+}
+export default Footer;
